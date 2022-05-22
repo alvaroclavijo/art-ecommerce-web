bbeda88d78af89391b43deb41df1fd8f92066bc8
Alvaro Clavijo <alvaro.clavijo.b21@gmail.com>
2022-05-22 13:46:30 -0400

    Web App > Database

---
+++ src/database/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE product (
+    product_id serial PRIMARY KEY,
+    name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2),
+    description VARCHAR(500),
+    recommendations JSON,
+    height INTEGER,
+    width INTEGER,
+    size  DECIMAL(10,1),
+    bestseller BOOLEAN,
+    featured BOOLEAN,
+    currency VARCHAR(6) DEFAULT 'USD',
+    image TEXT
+);
